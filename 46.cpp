adbd8f8d2158998dc393537b28acf0f08ca212b5
CSWangXY <wangxy_2000@163.com>
2022-10-10 21:29:26 +0800

    46

---
+++ 46.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<bool> used(n , false);
+        vector<int> path;
+        dfs(nums , used, path, 0);
+        return ans;
+    }
+
+    void dfs(vector<int>& nums , vector<bool>& used, vector<int> path, int depth)
+    {
+        if(depth == nums.size())
+        {
+            ans.push_back(path);
+            return;
+        }
+        for(int i=0 ; i<nums.size() ; i++)
+        {
+            if(used[i] == false)
+            {
+                path.push_back(nums[i]);
+                used[i] = true;
+                dfs(nums , used, path, depth+1);
+                used[i] = false;
+                path.pop_back();
+
+            }
+        }
+    }
+
+};
